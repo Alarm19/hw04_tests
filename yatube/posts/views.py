b834a6a5f68313c1bbee41febd5fc873de6e4d6c
Сергей Ли <li.sergey.99@mail.ru>
2023-04-13 23:47:58 +0300

    pytest пройден

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -73,16 +73,20 @@ def post_create(request):
 
 @login_required
 def post_edit(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    if post.author != request.user:
-        return redirect('posts:post_detail', post_id)
+    post = get_object_or_404(Post, id=post_id)
+    if post.author.id != request.user.id:
+        return redirect('posts:post_detail', post_id=post_id)
     form = PostForm(request.POST or None, instance=post)
     if form.is_valid():
         form.save()
-        return redirect('posts:post_detail', post_id)
+        return redirect('posts:post_detail', post_id=post_id)
     context = {
         'form': form,
-        'is_edit': True,
-        'post_id': post_id
+        'post': post_id,
+        'is_edit': True
     }
-    return render(request, 'posts/create_post.html', context)
+    return render(
+        request,
+        'posts/create_post.html',
+        context
+    )
